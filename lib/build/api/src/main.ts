d3c70e2d83f780ea4fed8c17940fe1a4c5b9270c
Drew Marshall <43978216+stinklewinks@users.noreply.github.com>
2025-05-23 13:39:50 -0500

    KiwiPress MVP

--- lib/build/api/src/main.ts
+++ lib/build/api/src/main.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { response } from 'express'
 import dotenv from 'dotenv'
 import WPData from './lib/wp-data'
 
@@ -9,11 +9,11 @@ const port = process.env.PORT || 3002
 
 
 
-app.get('/posts', (req, res) => {
+app.get('/posts', async (req, res) => {
   // fetch function of posts
   const post = new WPData();
-  const postData = post.getPostById(1, '/wp-json/wp/v2/posts');
-  res.send(postData);
+  const postData = await post.getPostById(1, 'http://wp_api/wp-json/wp/v2/posts/1');
+  res.json(postData);
 })
 
 app.get('/', (req, res) => {
